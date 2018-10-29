c27eb0f6225da3fe95d363495d50c43afc98bc66
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-29 18:00:54 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m1/1.1 Graph/Solution.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+// interface Graph {
+//     public int V();
+//     public int E();
+//     public void addEdge(int v, int w);
+//     public Iterable<Integer> adj(int v);
+//     public boolean hasEdge(int v, int w);
+// }
+
+class Graph {
+	int node;
+	int edge;
+	int ncount;
+	int ecount;
+	String[] tokens;
+	Bag<Integer>[] adjacent;
+	Graph(Scanner scan) {
+		node = scan.nextInt();
+		edge = scan.nextInt();
+		tokens = scan.nextLine().split(",");
+		adjacent = new Bag[node];
+		for (int i = 0; i < edge; i++) {
+			adjacent[i] = new Bag();
+		}
+		for (int j = 0; j < node; j++) {
+			String[] items = scan.nextLine().split(" ");
+			addEdge(Integer.parseInt(items[0]),
+			        Integer.parseInt(items[1]));
+		}
+	}
+	public void addEdge(int one, int two) {
+		ecount++;
+		adjacent[one].add(two);
+		adjacent[two].add(one);
+	}
+	public int getEdges() {
+		return ecount;
+	}
+	public int getNode() {
+		return ncount;
+	}
+	public Iterable<Integer> adjacent(int one) {
+		return adjacent[one];
+	}
+	public String toPrint() {
+		String str = "";
+		str += node + " vertices, " + edge + " edges " + "\n";
+		for (int k = 0; k < node; k++) {
+			str += k + ": ";
+			for (int in : adjacent[k]) {
+				str += in + " ";
+			}
+			str += "\n";
+		}
+        return str;
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String lismat = scan.nextLine();
+		Graph gob = new Graph(scan);
+		System.out.println(gob.toPrint());
+	}
+}
