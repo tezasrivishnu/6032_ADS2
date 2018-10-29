3914cd6ec60f744b2f194a6e2e88f04cb220254e
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-29 19:38:54 +0530

    "ads2_m1_a1_1: 6/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m1/1.1 Graph/Solution.java
+++ assignments/m1/1.1 Graph/Solution.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 //     public boolean hasEdge(int v, int w);
 // }
 
-class Graph {
+class ListGraph {
 	int node;
 	int edge;
 	int ncount;
 	int ecount;
 	String[] tokens;
 	Bag<Integer>[] adjacent;
-	Graph(Scanner scan) {
+	ListGraph(Scanner scan) {
 		node = Integer.parseInt(scan.nextLine());
 		edge = Integer.parseInt(scan.nextLine());
 		tokens = scan.nextLine().split(",");
@@ -67,12 +67,80 @@ class Graph {
 		return str;
 	}
 }
-
+class MatrixGraph {
+	int node;
+	int edge;
+	int ncount;
+	int ecount;
+	String[] tokens;
+	int[][] adjacent;
+	MatrixGraph(Scanner scan) {
+		node = Integer.parseInt(scan.nextLine());
+		edge = Integer.parseInt(scan.nextLine());
+		tokens = scan.nextLine().split(",");
+		adjacent = new int[node][node];
+		for (int i = 0; i < node; i++) {
+			for (int j = 0; j < node; j++) {
+				adjacent[i][j] = 0;
+			}
+		}
+		for (int j = 0; j < edge; j++) {
+			String[] items = scan.nextLine().split(" ");
+			addEdge(Integer.parseInt(items[0]),
+			        Integer.parseInt(items[1]));
+		}
+	}
+	public void addEdge(int one, int two) {
+		if (one != two) {
+			ecount++;
+			adjacent[one][two] = 1;
+			adjacent[two][one] = 1;
+		}
+	}
+	public int getEdges() {
+		return ecount;
+	}
+	public int getNode() {
+		return ncount;
+	}
+	// public int[] adjacent(int one) {
+	// 	return adjacent[one];
+	// }
+	public String toPrint() {
+		String str = node + " vertices, " + ecount + " edges" + "\n";
+		if (ecount > 0) {
+			int k = 0;
+			//str += node + " vertices, " + ecount + " edges" + "\n";
+			for (k = 0; k < adjacent.length; k++) {
+				for (int in = 0; in< adjacent[0].length; in++) {
+					str += adjacent[k][in] + " ";
+				}
+				str += "\n";
+			}
+			// str += tokens[k] + ": ";
+			// for (int in : adjacent[k]) {
+			// 	str += in + " ";
+			// }
+		} else {
+			str += "No edges";
+			return str;
+		}
+		return str;
+	}
+}
 class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String lismat = scan.nextLine();
-		Graph gob = new Graph(scan);
-		System.out.println(gob.toPrint());
+		switch (lismat) {
+		case "List":
+			ListGraph lis = new ListGraph(scan);
+			System.out.println(lis.toPrint());
+			break;
+		case "Matrix":
+			MatrixGraph mat = new MatrixGraph(scan);
+			System.out.println(mat.toPrint());
+			break;
+		}
 	}
 }
