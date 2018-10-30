432cd55647401647c9706f9e6f5eade269520f76
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-30 11:12:27 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m1/1.1 Graph/Solution.java
+++ assignments/m1/1.1 Graph/Solution.java
@@ -7,13 +7,39 @@ import java.util.Scanner;
 //     public boolean hasEdge(int v, int w);
 // }
 
+/**
+ * Class for list representation of graph.
+ */
 class ListGraph {
+	/**
+	 * intializing the vertices variable.
+	 */
 	private int node;
+	/**
+	 * intializing the edge variable.
+	 */
 	private int edge;
+	/**
+	 * intializing the vertices size variable.
+	 */
 	private int ncount;
+	/**
+	 * intializing the edges size variable.
+	 */
 	private int ecount;
+	/**
+	 * intializing the string array for vertices.
+	 */
 	private String[] tokens;
+	/**
+	 * intializing bag array.
+	 */
 	private Bag<Integer>[] adjacent;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      scan  The scan
+	 */
 	ListGraph(final Scanner scan) {
 		node = Integer.parseInt(scan.nextLine());
 		edge = Integer.parseInt(scan.nextLine());
@@ -28,6 +54,14 @@ class ListGraph {
 			        Integer.parseInt(items[1]));
 		}
 	}
+	/**
+	 *the method is to add an edge between.
+	 *two vertices.
+	  * complexity is O(E)
+	 * E denotes the number of edges we have in graph.
+	 * @param      One  The vertex one
+	 * @param      Two  The vertex two
+	 */
 	public void addEdge(final int one, final int two) {
 		if (one == two) {
 			return;
@@ -38,12 +72,30 @@ class ListGraph {
 		adjacent[one].add(two);
 		adjacent[two].add(one);
 	}
+	/**
+	 *returns the edges of graph.
+	 *
+	 * @return edges of graph
+	 */
 	public int getEdges() {
 		return ecount;
 	}
+	/**
+	 *returns the vertices of graph.
+	 *
+	 * @return vertices of graph
+	 */
 	public int getNode() {
 		return ncount;
 	}
+	/**
+	*the method is to return the adjacent vertices
+	 *in the bag.
+	 * complexity O(1)
+	* @param      one  The vertex
+	*
+	* @return  iterator.
+	*/
 	public Iterable<Integer> adjacent(final int one) {
 		return adjacent[one];
 	}
@@ -71,6 +123,16 @@ class ListGraph {
 		}
 		return str;
 	}
+	/**
+	*the method is check whether there is a.
+	*connection between two given vertices.
+	*complexity is O(E)
+	*E is the number of edges in graph.
+	* @param      One  The vertex one
+	* @param      Two  The vertex two
+	*
+	* @return     True if has edge, False otherwise.
+	*/
 	public boolean hasEdge(final int one, final int two) {
 		for (int in : adjacent(one)) {
 			if (in == two) {
@@ -80,13 +142,37 @@ class ListGraph {
 		return true;
 	}
 }
+
 class MatrixGraph {
+	/**
+	 * intializing the vertices variable.
+	 */
 	private int node;
+	/**
+	 * intializing the edge variable.
+	 */
 	private int edge;
+	/**
+	 * intializing the vertices size variable.
+	 */
 	private int ncount;
+	/**
+	 * intializing the edges size variable.
+	 */
 	private int ecount;
+	/**
+	 * intializing the string array for vertices.
+	 */
 	private String[] tokens;
+	/**
+	 * intializing the 2d array for edges.
+	 */
 	private int[][] adjacent;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      scan  The scan
+	 */
 	MatrixGraph(final Scanner scan) {
 		node = Integer.parseInt(scan.nextLine());
 		edge = Integer.parseInt(scan.nextLine());
@@ -103,6 +189,14 @@ class MatrixGraph {
 			        Integer.parseInt(items[1]));
 		}
 	}
+	/**
+	 *the method is to add an edge between.
+	 *two vertices.
+	 * @param      One  The vertex one
+	 * @param      Two  The vertex two
+	  * complexity is O(1) as we are just mking the index
+	  * as 1.
+	 */
 	public void addEdge(final int one, final int two) {
 		if (one == two) {
 			return;
@@ -113,19 +207,41 @@ class MatrixGraph {
 		adjacent[one][two] = 1;
 		adjacent[two][one] = 1;
 	}
+	/**
+	*the method is check whether there is a.
+	*connection between two given vertices.
+	*the time complexity is O(1)
+	* @param      One  The vertex one
+	* @param      Two  The vertex two
+	*
+	* @return     True if has edge, False otherwise.
+	*/
 	public boolean hasEdge(final int one, final int two) {
 		return adjacent[one][two] != 1
 		       && adjacent[two][one] != 1;
 	}
+	/**
+	 *returns the edges of graph.
+	 *
+	 * @return edges of graph
+	 */
 	public int getEdges() {
 		return ecount;
 	}
+	/**
+	 *returns the vertices of graph.
+	 *
+	 * @return vertices of graph
+	 */
 	public int getNode() {
 		return ncount;
 	}
-	public int[] adjacent(final int one) {
-		return adjacent[one];
-	}
+	/**
+	 *the method is to print the string format.
+	 *of graph.
+	 *the time complexity will be O(N^2)
+	 *N is the vertices here.
+	 */
 	public String toPrint() {
 		String str = node + " vertices, "
 		             + ecount + " edges" + "\n";
