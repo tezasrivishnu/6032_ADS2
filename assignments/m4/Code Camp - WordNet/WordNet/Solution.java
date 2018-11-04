7a9eddc7ab7886af4658df72928bfb273eac7027
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-11-04 11:19:02 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
-class Solution {
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String synsets = StdIn.readString();
-		String hypernyms = StdIn.readString();
-		String input = StdIn.readString();
-		try {
-			WordNet word = new WordNet(synsets, hypernyms);
-			if (input.equals("Graph")) {
-				System.out.println(word.print());
-			} else {
-				while (!StdIn.isEmpty()) {
-					String one = StdIn.readString();
-					String two = StdIn.readString();
-					System.out.println("distance = " +
-					                   word.distance(one, two) + ", ancestor = " +
-					                   word.sap(one, two));
-				}
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method for the program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String synsets = StdIn.readString();
+        String hypernyms = StdIn.readString();
+        String input = StdIn.readString();
+        try {
+            WordNet word = new WordNet(synsets, hypernyms);
+            if (input.equals("Graph")) {
+                System.out.println(word.print());
+            } else {
+                while (!StdIn.isEmpty()) {
+                    String one = StdIn.readString();
+                    String two = StdIn.readString();
+                    System.out.println("distance = "
+                    + word.distance(one, two) + ", ancestor = "
+                    + word.sap(one, two));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
