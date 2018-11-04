0d187c8f7f753eda1b5163033ea90d2193626b3a
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-11-04 13:10:52 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m4/Code Camp - WordNet/WordNet/BreadthFirstDirectedPaths.java
+++ assignments/m4/Code Camp - WordNet/WordNet/BreadthFirstDirectedPaths.java
@@ -1,62 +1,48 @@
-/******************************************************************************
- *  Compilation:  javac BreadthFirstDirectedPaths.java
- *  Execution:    java BreadthFirstDirectedPaths digraph.txt s
- *  Dependencies: Digraph.java Queue.java Stack.java
- *  Data files:   http://algs4.cs.princeton.edu/42digraph/tinyDG.txt
- *                http://algs4.cs.princeton.edu/42digraph/mediumDG.txt
- *                http://algs4.cs.princeton.edu/42digraph/largeDG.txt
- *
- *  Run breadth-first search on a digraph.
- *  Runs in O(E + V) time.
- *
- *  % java BreadthFirstDirectedPaths tinyDG.txt 3
- *  3 to 0 (2):  3->2->0
- *  3 to 1 (3):  3->2->0->1
- *  3 to 2 (1):  3->2
- *  3 to 3 (0):  3
- *  3 to 4 (2):  3->5->4
- *  3 to 5 (1):  3->5
- *  3 to 6 (-):  not connected
- *  3 to 7 (-):  not connected
- *  3 to 8 (-):  not connected
- *  3 to 9 (-):  not connected
- *  3 to 10 (-):  not connected
- *  3 to 11 (-):  not connected
- *  3 to 12 (-):  not connected
- *
- ******************************************************************************/
-
-
 /**
- *  The {@code BreadthDirectedFirstPaths} class represents a data type for finding
+ *  The {@code BreadthDirectedFirstPaths} class represents a data
+ *  type for finding
  *  shortest paths (number of edges) from a source vertex <em>s</em>
  *  (or set of source vertices) to every other vertex in the digraph.
  *  <p>
  *  This implementation uses breadth-first search.
  *  The constructor takes time proportional to <em>V</em> + <em>E</em>,
- *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
- *  It uses extra space (not including the digraph) proportional to <em>V</em>.
+ *  where <em>V</em> is the number of vertices and <em>E</em> is
+ *  the number of edges.
+ *  It uses extra space (not including the digraph) proportional
+ *  to <em>V</em>.
  *  <p>
- *  For additional documentation, 
- *  see <a href="http://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of 
+ *  For additional documentation,
+ *  see <a href="http://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
 public class BreadthFirstDirectedPaths {
-    private static final int INFINITY = Integer.MAX_VALUE;
-    private boolean[] marked;  // marked[v] = is there an s->v path?
-    private int[] edgeTo;      // edgeTo[v] = last edge on shortest s->v path
-    private int[] distTo;      // distTo[v] = length of shortest s->v path
-
     /**
-     * Computes the shortest path from {@code s} and every other vertex in graph {@code G}.
+     * int inifinity.
+     */
+    private static final int INFINITY = Integer.MAX_VALUE;
+    /**
+     * boolean array.
+     */
+    private boolean[] marked;
+    /**
+     * inr array.
+     */
+    private int[] edgeTo;
+    /**
+     * int array.
+     */
+    private int[] distTo;
+    /**
+     * Computes the shortest path from {@code s} and
+     * every other vertex in graph {@code G}.
      * @param G the digraph
      * @param s the source vertex
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public BreadthFirstDirectedPaths(Digraph G, int s) {
+    public BreadthFirstDirectedPaths(final Digraph G, final int s) {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
@@ -67,14 +53,16 @@ public class BreadthFirstDirectedPaths {
     }
 
     /**
-     * Computes the shortest path from any one of the source vertices in {@code sources}
+     * Computes the shortest path from any one of
+     * the source vertices in {@code sources}
      * to every other vertex in graph {@code G}.
      * @param G the digraph
      * @param sources the source vertices
      * @throws IllegalArgumentException unless each vertex {@code v} in
      *         {@code sources} satisfies {@code 0 <= v < V}
      */
-    public BreadthFirstDirectedPaths(Digraph G, Iterable<Integer> sources) {
+    public BreadthFirstDirectedPaths(final Digraph G,
+        final Iterable<Integer> sources) {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
@@ -85,7 +73,14 @@ public class BreadthFirstDirectedPaths {
     }
 
     // BFS from single source
-    private void bfs(Digraph G, int s) {
+
+    /**
+     * bfs method.
+     *complexity O(e) e is number of vertives.
+     * @param      G        { parameter_description }
+     * @param      sources  The sources
+     */
+    private void bfs(final Digraph G, final int s) {
         Queue<Integer> q = new Queue<Integer>();
         marked[s] = true;
         distTo[s] = 0;
@@ -104,7 +99,15 @@ public class BreadthFirstDirectedPaths {
     }
 
     // BFS from multiple sources
-    private void bfs(Digraph G, Iterable<Integer> sources) {
+
+    /**
+     * bfs method.
+     *complexity O(e) e is number of vertives.
+     * @param      G        { parameter_description }
+     * @param      sources  The sources
+     */
+    private void bfs(final Digraph G,
+        final Iterable<Integer> sources) {
         Queue<Integer> q = new Queue<Integer>();
         for (int s : sources) {
             marked[s] = true;
@@ -125,24 +128,27 @@ public class BreadthFirstDirectedPaths {
     }
 
     /**
-     * Is there a directed path from the source {@code s} (or sources) to vertex {@code v}?
+     * Is there a directed path from the source {@code s}
+     * (or sources) to vertex {@code v}?
      * @param v the vertex
-     * @return {@code true} if there is a directed path, {@code false} otherwise
+     * @return {@code true} if there is a directed path,
+     * {@code false} otherwise
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public boolean hasPathTo(int v) {
+    public boolean hasPathTo(final int v) {
         validateVertex(v);
         return marked[v];
     }
 
     /**
-     * Returns the number of edges in a shortest path from the source {@code s}
+     * Returns the number of edges in a shortest
+     * path from the source {@code s}
      * (or sources) to vertex {@code v}?
      * @param v the vertex
      * @return the number of edges in a shortest path
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public int distTo(int v) {
+    public int distTo(final int v) {
         validateVertex(v);
         return distTo[v];
     }
@@ -154,7 +160,7 @@ public class BreadthFirstDirectedPaths {
      * @return the sequence of vertices on a shortest path, as an Iterable
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public Iterable<Integer> pathTo(int v) {
+    public Iterable<Integer> pathTo(final int v) {
         validateVertex(v);
 
         if (!hasPathTo(v)) return null;
@@ -166,80 +172,34 @@ public class BreadthFirstDirectedPaths {
         return path;
     }
 
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
+    /**
+     *throw an IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    private void validateVertex(final int v) {
         int V = marked.length;
         if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+            throw new IllegalArgumentException("vertex "
+                 + v + " is not between 0 and " + (V - 1));
     }
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertices(Iterable<Integer> vertices) {
+
+    /**
+     * checking if vertexes are valid or not.
+     *
+     * @param      vertices  The vertices
+     */
+    private void validateVertices(final Iterable<Integer> vertices) {
         if (vertices == null) {
             throw new IllegalArgumentException("argument is null");
         }
         int V = marked.length;
         for (int v : vertices) {
             if (v < 0 || v >= V) {
-                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+                throw new IllegalArgumentException("vertex "
+                    + v + " is not between 0 and " + (V - 1));
             }
         }
     }
 
-
-    /**
-     * Unit tests the {@code BreadthFirstDirectedPaths} data type.
-     *
-     * @param args the command-line arguments
-     */
-    // public static void main(String[] args) {
-    //     In in = new In(args[0]);
-    //     Digraph G = new Digraph(in);
-    //     // StdOut.println(G);
-
-    //     int s = Integer.parseInt(args[1]);
-    //     BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(G, s);
-
-    //     for (int v = 0; v < G.V(); v++) {
-    //         if (bfs.hasPathTo(v)) {
-    //             StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
-    //             for (int x : bfs.pathTo(v)) {
-    //                 if (x == s) StdOut.print(x);
-    //                 else        StdOut.print("->" + x);
-    //             }
-    //             StdOut.println();
-    //         }
-
-    //         else {
-    //             StdOut.printf("%d to %d (-):  not connected\n", s, v);
-    //         }
-
-    //     }
-    // }
-
-
 }
-
-/******************************************************************************
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
