30ebc68460d4a62b46b6a37ad295327bb3c8fb2a
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-11-13 17:39:14 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m14/Assignment-1/Solution.java
+++ assignments/m14/Assignment-1/Solution.java
@@ -1,27 +1,62 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * main method for the program.
+	 * complexity O()
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		String[] words = loadWords();
 		Scanner scan = new Scanner(System.in);
 		String token = scan.nextLine();
 		TST tst = new TST();
 		for (String each : words) {
-            for (int i = 0; i < each.length(); i++) {
-            	tst.put(each.substring(i,each.length()), 0);
-            }
-        }
-        System.out.println(tst.keysWithPrefix(token));
+			for (int i = 0; i < each.length(); i++) {
+				tst.put(each.substring(i, each.length()), 0);
+			}
+		}
+		System.out.println(tst.keysWithPrefix(token));
 	}
+	/**
+	 * Loads words.
+	 * complexity O()
+	 * @return     array.
+	 */
 	public static String[] loadWords() {
 		In in = new In("/Files/dictionary-algs4.txt");
 		String[] words = in.readAllStrings();
 		return words;
 	}
 }
+/**
+ * Class for tst.
+ *
+ * @param      <Value>  The value
+ */
 class TST<Value> {
-	int n;
-	Node<Value> root;
+	/**
+	 * int variable.
+	 */
+	private int n;
+	/**
+	 * Node object.
+	 */
+	private Node<Value> root;
+	/**
+	 * Class for node.
+	 *
+	 * @param      <Value>  The value
+	 */
 	class Node<Value> {
 		char character;
 		Node<Value> left;
@@ -29,20 +64,47 @@ class TST<Value> {
 		Node<Value> right;
 		Value value;
 	}
+	/**
+	 * Constructs the object.
+	 */
 	TST() {
 
 	}
-	public boolean contains(String one) {
+	/**
+	 * checks if the string is present or not.
+	 * complexity O()
+	 * @param      one   One
+	 *
+	 * @return     boolean value.
+	 */
+	public boolean contains(final String one) {
 		return get(one) != null;
 	}
-	public Value get(String one) {
+	/**
+	 * helper method for the main get method.
+	 * comlexity O()
+	 * @param      one   One
+	 *
+	 * @return     the string.
+	 */
+	public Value get(final String one) {
 		Node<Value> node = get(root, one, 0);
 		if (node == null) {
 			return null;
 		}
 		return node.value;
 	}
-	public Node<Value> get(Node<Value> node, String one, int d) {
+	/**
+	 * it returns the value.
+	 * complexity O()
+	 * @param      node  The node
+	 * @param      one   One
+	 * @param      d     the character
+	 *
+	 * @return     node object.
+	 */
+	public Node<Value> get(final Node<Value> node,
+		final String one, final int d) {
 		if (node == null) {
 			return null;
 		}
@@ -57,12 +119,29 @@ class TST<Value> {
 			return node;
 		}
 	}
-	public void put(String one, Value value) {
+	/**
+	 * helper method for the main put method.
+	 * complexity O()
+	 * @param      one    One
+	 * @param      value  The value
+	 */
+	public void put(final String one, final Value value) {
 		if (!contains(one)) n++;
 		root = put(root, one, value, 0);
 	}
-
-	public Node<Value> put(Node<Value> node, String one, Value value, int d) {
+	/**
+	 * used to put the string with the assigned value.
+	 * complexity O()
+	 * @param      node   The node
+	 * @param      one    One
+	 * @param      value  The value
+	 * @param      d      the charcter index.
+	 *
+	 * @return     Node object.
+	 */
+	public Node<Value> put(Node<Value> node,
+		final String one, final Value value,
+		final int d) {
 		char ch = one.charAt(d);
 		if (node == null) {
 			node = new Node<Value>();
@@ -79,7 +158,14 @@ class TST<Value> {
 		}
 		return node;
 	}
-	public Iterable<String> keysWithPrefix(String one) {
+	/**
+	 * used to find the string with the prefix.
+	 * complexity O()
+	 * @param      one   One
+	 *
+	 * @return    Iterable.
+	 */
+	public Iterable<String> keysWithPrefix(final String one) {
 		Queue<String> que = new Queue<String>();
 		Node<Value> x = get(root, one, 0);
 		if (x == null) {
@@ -91,12 +177,23 @@ class TST<Value> {
 		collect(x.middle, new StringBuilder(one), que);
 		return que;
 	}
-	private void collect(Node<Value> node, StringBuilder one, Queue<String> que) {
-        if (node == null) return;
-        collect(node.left,  one, que);
-        if (node.value != null) que.enqueue(one.toString() + node.character);
-        collect(node.middle, one.append(node.character), que);
-        one.deleteCharAt(one.length() - 1);
-        collect(node.right, one, que);
-    }
+	/**
+	 * checks for the strings with the preffix
+	 * in trie.
+	 * complexity O()
+	 * @param      node  The node
+	 * @param      one   One
+	 * @param      que   The que
+	 */
+	public void collect(final Node<Value> node,
+		final StringBuilder one, final Queue<String> que) {
+		if (node == null) return;
+		collect(node.left,  one, que);
+		if (node.value != null) que.enqueue(one.toString()
+			+ node.character);
+		collect(node.middle, one.append(
+			node.character), que);
+		one.deleteCharAt(one.length() - 1);
+		collect(node.right, one, que);
+	}
 }
